b0cba113a68569e9933a299b7dd3aef518d4bd07
Winicios <winiciosmauri19@hotmail.com>
2022-11-17 22:22:34 -0300

    Feat: Create Model - Create Route

---
+++ src/app/models/Order.ts
@@ -0,0 +1,38 @@
+import { model, Schema } from 'mongoose';
+
+export const Order = model(
+  'Order',
+  new Schema({
+    table: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      enum: [
+        'WAITING',
+        'IN_PRODUCTION',
+        'DONE'
+      ],
+      default: 'WAITING'
+    },
+    createsAt: {
+      type: Date,
+      default: Date.now,
+    },
+    product:{
+      required: true,
+      type: [{
+        product: {
+          type: Schema.Types.ObjectId,
+          required: true,
+          ref: 'Product',
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        }
+      }]
+    }
+  })
+);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-const app = express();
+
 
 mongoose.connect('mongodb://127.0.0.1:27017')
-  .then(() => console.log('Conectado com sucesso!'))
-  .catch(() => console.log(Error));
+  .then(() => {
+    const app = express();
+    const port = 3001;
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log('Erro ao conectar no Banco de Dados'));
 
-const port = 3001;
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+
+
